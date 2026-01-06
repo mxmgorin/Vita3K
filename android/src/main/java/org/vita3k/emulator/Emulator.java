20d235487f6d64da70985a6cdf4a4a13e354c7c0
Troidem <onlinexmax@gmail.com>
2026-01-06 23:10:00 +0300

    android: hide system bars

--- android/src/main/java/org/vita3k/emulator/Emulator.java
+++ android/src/main/java/org/vita3k/emulator/Emulator.java
@@ -6,6 +6,7 @@ import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Build;
 import android.os.Environment;
 import android.os.ParcelFileDescriptor;
@@ -14,6 +15,7 @@ import android.system.ErrnoException;
 import android.system.Os;
 import android.view.Surface;
 import android.view.ViewGroup;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Keep;
@@ -62,6 +64,26 @@ public class Emulator extends SDLActivity
     @Override
     protected String[] getLibraries() {
         return new String[] { "Vita3K" };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideSystemBars();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideSystemBars();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemBars();
+        }
     }
 
     @Override
@@ -329,4 +351,15 @@ public class Emulator extends SDLActivity
     }
 
     public native void filedialogReturn(String result_path, int result_fd);
+
+    private void hideSystemBars() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+    }
 }
